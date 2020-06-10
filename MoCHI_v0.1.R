9a3df9c486a2d03415a4675af35b07aefb28fe3c
andrefaure <a.jfaure@gmail.com>
2020-06-10 15:59:49 +0200

    update wrapper

--- MoCHI_v0.1.R
+++ MoCHI_v0.1.R
@@ -13,6 +13,8 @@ option_list <- list(
   optparse::make_option(opt_str=c("--stopStage", "-t"), type="integer", default=0, help = "Stop at a specified pipeline stage (default: 0 i.e. no stop condition)"),
   optparse::make_option(opt_str=c("--numCores", "-c"), type="integer", default=1, help = "Number of available CPU cores"),
   optparse::make_option(opt_str=c("--maxOrder"), type="integer", default=2, help = "Maximum number of nucleotide or amino acid substitutions for coding or non-coding sequences respectively (default:2)")
+  optparse::make_option(opt_str=c("--numReplicates"), type="integer", default=2, help = "Number of biological replicates (or sample size) from which fitness and error estimates derived (default:2)")
+  optparse::make_option(opt_str=c("--FDR_threshold"), type="double", default=0.1, help = "FDR threshold for significant specific and background averaged terms (default:0.1)")
 )
 
 arg_list <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
@@ -36,4 +38,6 @@ mochi(
   startStage=arg_list[["startStage"]],
   stopStage=arg_list[["stopStage"]],
   numCores=arg_list[["numCores"]],
-  maxOrder=arg_list[["maxOrder"]])
+  maxOrder=arg_list[["maxOrder"]],
+  numReplicates=arg_list[["numReplicates"]],
+  FDR_threshold=arg_list[["FDR_threshold"]])
